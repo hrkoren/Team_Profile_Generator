e9f2b365a4133bd0d5108989f50a8946e6280848
HRush <hrkoren@gmail.com>
2021-03-31 19:57:58 -0400

    updated code with switch cases and functions to pull different questions based on selections

--- Dev/team_profile.js
+++ Dev/team_profile.js
@@ -7,7 +7,6 @@ function generateHTML(data) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="./Dev/style.css">
     <title>Team Profile Generator</title>
     <style>
     .container-md {
--- index.js
+++ index.js
@@ -1,93 +1,156 @@
 //Packages needed for application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const { endianness } = require('os');
 const employee = require('./Dev/Employee.js');
 const engineer = require('./Dev/Engineer');
 const intern = require('./Dev/Intern');
 const manager = require('./Dev/Manager');
 const generateHTML = require('./Dev/team_profile.js');
-const style = require('.Dev/style.css');
 
 //Array of prompts for user input
 const profilePrompts = [];
+function managerPrompt() {
 inquirer
     .prompt([
         {
             type: 'input',
             message: 'Please enter the name of the team manager.',
             name: 'teamManager',
-            validate: (value) => { if (value) { return true} else { return 'Please enter the team manager.'} },
+            validate: (value) => { if (value) { return true } else { return 'Please enter the team manager.' } },
         },
         {
             type: 'input',
             message: 'Please enter the employee ID for the team manager.',
             name: 'managerID',
-            validate: (value) => { if (value) { return true} else { return 'Please enter an employee ID.'} },
+            validate: (value) => { if (value) { return true } else { return 'Please enter an employee ID.' } },
         },
-         {
+        {
             type: 'input',
             message: 'Please enter the email address for the team manager.',
             name: 'managerEmail',
-            validate: (value) => { if (value) { return true} else { return 'Please enter the email address for the team manager.'} },
+            validate: (value) => { if (value) { return true } else { return 'Please enter the email address for the team manager.' } },
         },
         {
             type: 'input',
             message: 'Please enter the phone number for the team manager.',
             name: 'managerPhone',
-            validate: (value) => { if (value) { return true} else { return 'Please enter the phone number for the team manager.'} },
-        },
+            validate: (value) => { if (value) { return true } else { return 'Please enter the phone number for the team manager.' } },
+        }]).then(() => {
+            employeeChoice(); 
+        })
+    }
+
+
+    function employeeChoice() {
+        inquirer
+        .prompt([  
         {
-            type: 'checkbox',
+            type: 'list',
             message: 'What type of employee do you need to add?',
             name: 'employeeType',
             choices: ['Engineer', 'Intern', 'Finish Building My Team'],
-            validate: (value) => { if (value) { return true} else { return 'Please select an option.'} },
-        },
-        {
-            type: 'input',
-            message: 'Please enter the name of the Engineer.',
-            name: 'engName',
-            validate: (value) => { if (value) { return true} else { return 'Please enter the name for the new team member.'} },
-        },
-        {
-            type: 'input',
-            message: 'Please enter the name of the Intern.',
-            name: 'internName',
-            validate: (value) => { if (value) { return true} else { return 'Please enter the name for the new team member.'} },
-        },
-        {
-            type: 'input',
-            message: 'Please enter the employee ID.',
-            name: 'employeeID',
-            validate: (value) => { if (value) { return true} else { return 'Please enter an employee ID.'} },
-        },
-         {
-            type: 'input',
-            message: 'Please enter the email address for the team member.',
-            name: 'employeeEmail',
-            validate: (value) => { if (value) { return true} else { return 'Please enter the email address for the team member.'} },
-        },
-        {
-            type: 'input',
-            message: 'Please enter the GitHub username for the team member.',
-            name: 'github',
-            validate: (value) => { if (value) { return true} else { return 'Please enter the GitHub username for the team member.'} },
-        },
-        {
-            type: 'input',
-            message: 'Please enter the school name for the Intern.',
-            name: 'school',
-            validate: (value) => { if (value) { return true} else { return 'Please enter the school for the team member.'} },
-        },
-    ])
-    .then(data => {
-        const employeeProfile = generateHTML(data);
-        console.log(data);
-
-        fs.writeFile('index.html', employeeProfile, (error) => {
-            if(error) {
-                console.log(error)
+            validate: (value) => { if (value) { return true } else { return 'Please select an option.' } },
+        }]).then(data => {
+            switch (data.employeeType) {
+                case 'Engineer':
+                    return engineerChoice();
+                case 'Intern':
+                    return internChoice();
+                case 'Finish Building My Team':
+                    return stopChoice;
+                default:
+                    break;
             }
-        });
-    })
+            console.log(data);
+        })
+    }
 
+function engineerChoice() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Please enter the name of the Engineer.',
+                name: 'engName',
+                validate: (value) => { if (value) { return true } else { return 'Please enter the name for the new team member.' } },
+            },
+            {
+                type: 'input',
+                message: 'Please enter the employee ID.',
+                name: 'employeeID',
+                validate: (value) => { if (value) { return true } else { return 'Please enter an employee ID.' } },
+            },
+            {
+                type: 'input',
+                message: 'Please enter the email address for the team member.',
+                name: 'employeeEmail',
+                validate: (value) => { if (value) { return true } else { return 'Please enter the email address for the team member.' } },
+            },
+            {
+                type: 'input',
+                message: 'Please enter the GitHub username for the team member.',
+                name: 'github',
+                validate: (value) => { if (value) { return true } else { return 'Please enter the GitHub username for the team member.' } },
+            },
+        ]).then(info => {
+            employeeChoice();
+            console.log(info);
+        });
+}
+
+function internChoice() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Please enter the name of the Intern.',
+                name: 'internName',
+                validate: (value) => { if (value) { return true } else { return 'Please enter the name for the new team member.' } },
+            },
+            {
+                type: 'input',
+                message: 'Please enter the employee ID.',
+                name: 'employeeID',
+                validate: (value) => { if (value) { return true } else { return 'Please enter an employee ID.' } },
+            },
+            {
+                type: 'input',
+                message: 'Please enter the email address for the team member.',
+                name: 'employeeEmail',
+                validate: (value) => { if (value) { return true } else { return 'Please enter the email address for the team member.' } },
+            },
+            {
+                type: 'input',
+                message: 'Please enter the GitHub username for the team member.',
+                name: 'github',
+                validate: (value) => { if (value) { return true } else { return 'Please enter the GitHub username for the team member.' } },
+            },
+            {
+                type: 'input',
+                message: 'Please enter the school name for the Intern.',
+                name: 'school',
+                validate: (value) => { if (value) { return true } else { return 'Please enter the school for the team member.' } },
+            },
+        ])
+    }
+            // fs.writeFile('index.html', employeeProfile, (error) => {
+            //     if(error) {
+            //         console.log(error)
+            //     }
+            // });
+
+
+    //   
+    // .then(data => {
+    //     const employeeProfile = generateHTML(data);
+    //     console.log(data);
+
+    //     fs.writeFile('index.html', employeeProfile, (error) => {
+    //         if(error) {
+    //             console.log(error)
+    //         }
+    //     });
+
+
+    managerPrompt();
